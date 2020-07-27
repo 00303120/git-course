9e582e77ed8410c50e148016215c41eee7dd462d
eortiz <00303120@uca.edu.sv>
2020-07-26 21:04:54 -0600

    Add saludar function to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+void saludar();
+
 int main(void){
     
     cout << "Hola Mundo !! " << endl;
+    saludar();
+}
+
+void saludar(){
+    cout << "Elmer te saluda !! " << endl;
+
 }
